e27c507a3aed671c798a488dd30af1f93df88508
Vstrevalovo <berejan97@mail.ru>
2024-05-21 22:13:57 +0300

    Add files via upload

---
+++ Lesson0.py
@@ -0,0 +1,32 @@
+print('zadanie 1')
+a='privet'
+print(len(a))
+
+print('zadanie 2')
+first=9
+second=1
+summa=first+second
+print(summa)
+diff=first-second
+print(diff)
+
+print('zadanie 3')
+first=5
+second=3
+third=10
+count=[first, second, third]
+count=len(count)
+mean=(first+second+third)/count
+print(mean)
+
+print ('zadanie 4')
+first_string='Вторник'
+second_string='Понедельник'
+print(second_string, first_string)
+
+print('zadanie 5')
+a=5
+b=3
+c=7
+f=(a * b) + (a * c)
+print((f**3)/2)
